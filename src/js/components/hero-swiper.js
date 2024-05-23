32d36442b1a25dc13fb0a92cb58cab72130fcd12
Igor <k007.igor@gmail.com>
2024-05-23 18:42:59 +0500

    section hero + mobile version

---
+++ src/js/components/hero-swiper.js
@@ -0,0 +1,18 @@
+import Swiper from 'swiper';
+import { EffectFade, Autoplay } from 'swiper/modules';
+
+const swiper = new Swiper('.hero__swiper', {
+  modules: [EffectFade, Autoplay],
+
+  direction: 'vertical',
+  loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  crossFade: "true",
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
